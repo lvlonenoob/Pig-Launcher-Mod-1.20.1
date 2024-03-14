0da424887d200f32ee2924552f66a286bcf70015
lvlonenoob <thomasjn2006@gmail.com>
2024-03-14 21:50:31 +0100

    gbuyguy

--- src/main/java/com/rellaxi/piglauncher/item/custom/PigLauncherItem.java
+++ src/main/java/com/rellaxi/piglauncher/item/custom/PigLauncherItem.java
@@ -4,17 +4,54 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 
 public class PigLauncherItem extends Item {
     private boolean loaded;
+    private int ticker;
     public PigLauncherItem(Properties pProperties) {
         super(pProperties);
         loaded = false;
+        ticker = 0;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if (loaded) {
+            if (ticker > 0) {
+                ticker--;
+            }
+        }
+        else {
+            ticker = 20;
+        }
+        super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if (ticker == 0) {
+            if (loaded) {
+                ArrowItem arrowitem = (ArrowItem) (Items.ARROW);
+                AbstractArrow abstractarrow = arrowitem.createArrow(pLevel, new ItemStack(Items.ARROW), pPlayer);
+                abstractarrow.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 3.0F, 1.0F);
+                abstractarrow.pickup = AbstractArrow.Pickup.DISALLOWED;
+                pLevel.addFreshEntity(abstractarrow);
+                loaded = false;
+                toTag(pPlayer.getItemInHand(pUsedHand), loaded);
+            }
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @Override
@@ -22,7 +59,7 @@ public class PigLauncherItem extends Item {
         if (!pInteractionTarget.level().isClientSide()) {
             if (pInteractionTarget.getType() == EntityType.PIG) {
                 pInteractionTarget.discard();
-                loaded = !loaded;
+                loaded = true;
                 toTag(pPlayer.getItemInHand(pUsedHand), loaded);
             }
         }
